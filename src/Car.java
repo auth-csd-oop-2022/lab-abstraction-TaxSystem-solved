c3f3556bee1db67504618be4f20c67291ade7105
John Doe <gmeditsk@csd.auth.gr>
2021-11-28 16:22:08 +0200

    fix comments

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,6 @@
 /**
- * Αυτή η κλάση αναπαριστά ένα αυτοκίνητο. Κάθε αυτοκίνητο έχει εναν ακέραιο αριθμό που δηλώνει τον κυβισμό του
- * έναν αριθμό που δηλώνει την κατανάλοση του σε λίτρα/100 χιλιόμετρα και έναν αριθμό που δηλώνει την ιπποδύναμη του
+ * Αυτή η κλάση αναπαριστά ένα αυτοκίνητο. Κάθε αυτοκίνητο έχει έναν ακέραιο αριθμό που δηλώνει τον κυβισμό του έναν
+ * αριθμό που δηλώνει την κατανάλωση του σε λίτρα/100 χιλιόμετρα και έναν αριθμό που δηλώνει την ιπποδύναμη του
  * <p>
  * This class represents a car. Each car has a number that represents its cubic capacity, another one for its fuel
  * consumption in liters/100 km and a final number representing its horse power
@@ -14,33 +14,35 @@ public class Car {
     /**
      * Κατασκευαστής/Constructor
      */
-    public Car(int cubic_capacity, double consumption, int horse_power){
-        this.cubic_capacity=cubic_capacity;
-        this.consumption=consumption;
-        this.horse_power=horse_power;
+    public Car(int cubic_capacity, double consumption, int horse_power) {
+        this.cubic_capacity = cubic_capacity;
+        this.consumption = consumption;
+        this.horse_power = horse_power;
     }
 
-    public int getCubic_capacity(){
+    public int getCubic_capacity() {
         return this.cubic_capacity;
 
     }
-    public double getConsumption(){
+
+    public double getConsumption() {
 
         return this.consumption;
     }
-    public int getHorse_power(){
+
+    public int getHorse_power() {
 
         return this.horse_power;
     }
 
     /**
-     * H μέθοδος αυτή επιστρέφει αν το αυτοκίνητο είναι πολυτελές ή οχι. Ένα αυτοκίνητο θεωρείται πολυτελές
-     * αν ο κυβισμός του είναι μεγαλύτερος ή ισος του 2000.
-     * This method returns if a car is luxurious or not. A car is considered luxurious if its cubic capacity is
-     * higher than or equal to 2000.
-     * @return True αν είναι πολυτελές αλλίως False/ True if Luxurious else False
+     * H μέθοδος αυτή επιστρέφει αν το αυτοκίνητο είναι πολυτελές ή οχι. Ένα αυτοκίνητο θεωρείται πολυτελές αν ο
+     * κυβισμός του είναι μεγαλύτερος ή ίσος του 2000. This method returns if a car is luxurious or not. A car is
+     * considered luxurious if its cubic capacity is higher than or equal to 2000.
+     *
+     * @return True αν είναι πολυτελές αλλιώς False/ True if Luxurious else False
      */
-    public boolean isLuxurious(){
+    public boolean isLuxurious() {
         return (this.cubic_capacity >= 2000);
     }
 }
--- src/House.java
+++ src/House.java
@@ -1,9 +1,9 @@
 /**
- * Αυτη η κλάση αναπαριστά ένα σπίτι. Κάθε σπίτι έχει το μέγεθος του σε τετραγωνικά μέτρα, το αριθμό των ορόφων του,
- * τον αριθμό των δωματιών του και την ηλικία του σε χρόνια
+ * Αυτή η κλάση αναπαριστά ένα σπίτι. Κάθε σπίτι έχει το μέγεθος του σε τετραγωνικά μέτρα, το αριθμό των ορόφων του, τον
+ * αριθμό των δωματίων του και την ηλικία του σε χρόνια
  * <p>
- * This class represents a house. Each house has its size in square meters, the number of floors, the number of
- * rooms and the age of the house in years
+ * This class represents a house. Each house has its size in square meters, the number of floors, the number of rooms
+ * and the age of the house in years
  */
 
 public class House {
@@ -15,43 +15,43 @@ public class House {
     /**
      * Κατασκευαστής/Constructor
      */
-    public House(double square_meters, int floors, int rooms,int house_age){
+    public House(double square_meters, int floors, int rooms, int house_age) {
         this.square_meters = square_meters;
-        this.floors= floors;
-        this.rooms= rooms;
-        this.house_age=house_age;
+        this.floors = floors;
+        this.rooms = rooms;
+        this.house_age = house_age;
     }
 
-    public double getSquare_meters(){
+    public double getSquare_meters() {
 
         return this.square_meters;
     }
 
-    public int getFloors(){
+    public int getFloors() {
 
         return this.floors;
     }
 
-    public int getRooms(){
+    public int getRooms() {
 
         return this.rooms;
     }
 
-    public int getHouse_age(){
+    public int getHouse_age() {
 
         return this.house_age;
     }
 
     /**
-     * Η μέθοδος αυτη επιστρέφει αν ένα σπίτι είναι πολυτελές ή οχι. Ένα σπίτι θεωρείται πολυτελές αν
-     * ο αριθμός δωματιών του είναι μεγαλύτερος ή ίσος του 8.
-     * This method returns if a house is luxurious or not. A house is considered luxurious if the number of
-     * rooms is higher than or equal to 8.
+     * Η μέθοδος αυτή επιστρέφει αν ένα σπίτι είναι πολυτελές ή οχι. Ένα σπίτι θεωρείται πολυτελές αν ο αριθμός δωματίων
+     * του είναι μεγαλύτερος ή ίσος του 8. This method returns if a house is luxurious or not. A house is considered
+     * luxurious if the number of rooms is higher than or equal to 8.
+     *
      * @return True αν το σπίτι είναι πολυτελές False αν όχι/ True if the house is luxurious, False if not
      */
-    public boolean isLuxurious(){
+    public boolean isLuxurious() {
 
-        return (this.rooms >=8);
+        return (this.rooms >= 8);
     }
 
 }
